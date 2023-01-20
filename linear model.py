f3634f4b35451f7f7fea53436be9f44ec99968c6
Yugdeep Parihar <106156641+YugdeepParihar@users.noreply.github.com>
2023-01-20 21:16:12 +0530

    Add files via upload

---
+++ linear model.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+# Importing the dataset
+dataset = pd.read_excel('ipl.xlsx')
+X = dataset.iloc[:,[7,8,9,12,13]].values
+y = dataset.iloc[:, 14].values
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+y_train.shape
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# Training the dataset
+from sklearn.linear_model import LinearRegression
+lin = LinearRegression()
+lin.fit(X_train,y_train)
+
+import pickle
+pickle.dump(lin,open("model.pkl","wb"))
+
+import os
+os.getcwd()
+
+
+
+
